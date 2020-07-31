a361e1038e85e39c77d543498a1d3c05a4eebfb4
Thomas <thomas.gallian@outlook.com>
2020-07-31 19:51:50 +0200

    Chore: added QuickUnion and Weighted QuickUnion

--- OnLineConnectivity/OnLineConnectivity.c
+++ OnLineConnectivity/OnLineConnectivity.c
@@ -52,3 +52,52 @@ int single_Quick_find(int n, int id[n], int p, int q, int connect){
     printf("error");
     return -1;
 }
+
+void quick_union(int n) {
+    int i, j, p, q, id[n];
+    //init the id array: all elements point to themselves
+    for (i = 0; i < n; i++)
+        id[i] = i;
+
+    printf("input pair p, q: ");
+
+    while (scanf("%d %d", &p, &q) == 2) {
+        for (i = p; i != id[i]; i = id[i]);
+        for (i = p; i != id[i]; i = id[i]);
+        if (i == j) {
+            printf("pair already connected");
+        } else {
+            id[i] = j;
+            printf("pair not connected");
+        }
+        printf("input pair p q: ");
+    }
+}
+
+void Weighted_Quick_Union(int n){
+    int p,q,i,j,id[n],sz[n];
+
+    for (i = 0; i < n; i++){
+        id[i] = i;
+        sz[i]=i;
+    }
+    printf("input pair p q:");
+    while(scanf("%d %d", &p, &q)==2){
+        for (i = p; i != id[i]; i = id[i]);
+        for (i = p; i != id[i]; i = id[i]);
+        if(i==j){
+            printf("pair already connected \n");
+        }   else{
+                printf("pair not yet connected");
+                if(sz[i]<=sz[j]){
+                    id[i]=j;
+                    sz[j]+=sz[i];
+                } else{
+                    id[j]=i;
+                    sz[i] += sz[j];
+                }
+            }
+        printf ("input pair p q:");
+        }
+    }
+
--- OnLineConnectivity/OnLineConnectivity.h
+++ OnLineConnectivity/OnLineConnectivity.h
@@ -11,10 +11,12 @@
 /**
  * Online connectivity: given two integers p and q, this problem has as a goal to define wheter they
  * are connected or not.
+ * for every couple of points, these algorithms do:
+ * 1.Read p and q 2. find p's id  3. find q's id 4. if they are connected ask for next couple. If they aren't, make a union.
+ *
  * Quick-find algorithm saves the rappresentation of all the points on a id[n] array, finds if the two requested
  * points are already connected, and if they aren't it connects them.
- * It outputs their status (connected/not connected). If they weren't connected, it connects them.
- * initially id[i]=i: every point is connected to itself
+ * Initially id[i]=i: every point is connected to itself
  * when p and q are connected, id[p]=id[q]
  * Cost: Linear.
  * @param n max size of the key vector: to collect n elements, this parameter is n-1
@@ -22,12 +24,18 @@
 void quick_find(int n);
 /**
  * Single version of quick find. the id array, p, and q are given by input from external functions.
- *Online connectivity: given two integers p and q, this problem has as a goal to define wheter they
- * are connected or not.
+ *
  * Quick-find algorithm saves the rappresentation of all the points on a id[n] array, finds if the two requested
  * points are already connected, and if they aren't it connects them.
  * It outputs their status (connected/not connected). If they weren't connected, it connects them.
+ *
  * Cost: Linear.
+ * Online connectivity: given two integers p and q, this problem has as a goal to define wheter they
+ * are connected or not.
+ * for every couple of points, these algorithms do:
+ * 1.Read p and q 2. find p's id  3. find q's id 4. if they are connected ask for next couple. If they aren't, make a union.
+ *
+
  *
  * @param n size of the id array
  * @param id array containing the elements
@@ -38,8 +46,35 @@ void quick_find(int n);
  */
 int single_Quick_find(int n, int id[n], int p, int q, int connect);
 
+/**
+ * * Online connectivity: given two integers p and q, this problem has as a goal to define wheter they
+ * are connected or not.
+ * for every couple of points, these algorithms do:
+ * 1.Read p and q 2. find p's id  3. find q's id 4. if they are connected ask for next couple. If they aren't, make a union.
+ *
+ * QuickUnion is nice doesnt use cycles. It does:
+ * 1. initialize id[i]=i: every point points to itself
+ * 2. find: eerhy point is searched as a chain of links.
+ * 3. Union
+ *
+ * Cost: unitary
+ *
+ * @param n size of the id array
+ */
 void quick_union(int n);
 
-void Weighted_Quick_Union();
+/** Online connectivity: given two integers p and q, this problem has as a goal to define wheter they
+ * are connected or not.
+ * for every couple of points, these algorithms do:
+ * 1.Read p and q 2. find p's id  3. find q's id 4. if they are connected ask for next couple. If they aren't, make a union.
+ *
+ * When applying the union, Weighted Quick Union looks for the ensemble containing the most of points (data about the sizes
+ * is stored in the sz arrau), and connects only its root to the root of the bigger ensemble.
+ * root to the other.
+ * Weighted quick union
+ * @param n size of id and sz
+ */
+
+void Weighted_Quick_Union(int n);
 
 #endif //APA_BASICS_ONLINECONNECTIVITY_H
